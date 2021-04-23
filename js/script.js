ac0cdd02459c2b4dcfb82b90a2ef8d435d95508d
Anna <glikin.anna@gmail.com>
2021-04-24 00:41:49 +0300

    start js movie code

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 "use strict";
 
+
+const numberOfFilms = +prompt("how many films...?","");
+
+const personalMovieDB = {
+
+    count: numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+
+};
+
+const   a = prompt('one of the latest films?',''),
+        b = prompt("rate it", ''),
+        c = prompt('one of the latest films?',''),
+        d = prompt("rate it", '');
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+
+console.log(personalMovieDB);
